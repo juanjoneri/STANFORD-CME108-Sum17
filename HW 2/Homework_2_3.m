2432dd94af65db085c4584dcf14c24791c687cca
Juan Neri <Juanjo.neri@gmail.com>
2017-07-12 15:34:45 -0700

    make function that will generate lagrange polynomials

--- HW 2/Homework_2_3.m
+++ HW 2/Homework_2_3.m
@@ -1,6 +1,6 @@
 %% Problem 3
 
-syms t
+syms t;
 
 % Part a
 
@@ -8,14 +8,7 @@ syms t
 X = [1978:2:1992] - 1976;
 Y = [12, 12.7, 13, 15.2, 18.2, 19.8, 24.1, 28.1];
 
-A = [];
-for i = 1:8
-    A = [A ; row(X(i), 7)];
-end
-
-x = A\Y';
-T = row (t, 7);
-P = T * x;
+P = Lagrange(X, Y)
 
 XX = linspace(2, 16);
 y1 = vpa (subs( P, t, XX));
@@ -40,6 +33,29 @@ legend('Data Point','Cubic Spline')
 title('Part b')
 
 fprintf('Estimate for 1994 using Cubic Spline: %f \n', spline(x,y,1994));
+
+% Function Definitions
+
+function [ LP ] = Lagrange ( X, Y )
+    t = sym('t');
+    order = length(X);
+    A = [];
+    for i = 1:order
+        A = [A ; row(X(i), order - 1)];
+    end
+    
+    x = A\Y';
+    T = row (t, order - 1);
+    LP = T * x;
+    
+    function [ A ] = row ( x, order )
+        A = [];
+        for j = 0:order
+            A = [A, x^j];
+        end
+    end
+end
+
 function [ A ] = row ( x, order )
     A = [];
     for i = 0:order
