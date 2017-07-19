9162eacaff2fd9c1d7875c03cae119f955b6162a
Juan Neri <Juanjo.neri@gmail.com>
2017-07-19 13:53:52 -0700

    make a function to swap row with element in diagonal with highest from lower triangle

---
+++ HW 3/Homework_3_1.m
@@ -0,0 +1,51 @@
+%% Problem 1
+
+A = [ 2  5 -9  3
+      5  6 -4  2
+      3 -4  2  7
+      11 7  4 -8 ];
+ 
+ b = [ 151
+       103
+        16
+       -32 ];
+   
+ GaussEliminate(A)
+
+% Gauss Elimination
+function [ A ] = GaussEliminate ( A )
+    n = size(A, 1);
+    for k = 1:n-1 % elimination passes
+        % make pos kk be largest from column k in lower diag
+        A = maxPivot(A, k);
+        
+        for i = k+1:n % rows
+            tmp = A(i,k)/A(k,k);
+            for j=1:n
+                A(i,j) = A(i,j)-tmp*A(k,j);
+            end
+        end
+    end
+end
+
+function [ A ] = maxPivot ( A, k )
+    n = size(A, 1);
+    k_ = A(k,:);
+    
+    [M, I] = max(A(k:n, k));
+    j = k + I - 1;
+    j_ = A(j,:);
+    
+    
+    A(k,:) = j_;
+    A(j,:) = k_;
+end
+
+% Back Substitution
+function [ x ] = BackSubstitute ( A, b )
+    x = zeros(1, n);
+    x(n) = b(n)/A(n,n);
+    for i=n-1:-1:1
+        x(i) = (b(n)-sum(A(i,i+1:n).*x(i+1:n)))/A(i,i);
+    end
+end
--- Sandbox.m
+++ Sandbox.m
@@ -1,7 +1,24 @@
-a = 2;
-a = a - 2;
-A = [a a a]
-a = a + 3
-A'
+A = magic(4)
+maxPivot ( A, 3 )
 
-plot(A + 1:3)
+%[M, I] = max(A(2:4,1))
+
+function [ A ] = swapCol ( A, i, j )
+    k = A(i,:);
+    k_lg = A(j,:);
+    A(i,:) = k_lg;
+    A(j,:) = k;
+end
+
+function [ A ] = maxPivot ( A, k )
+    n = size(A, 1);
+    k_ = A(k,:);
+    
+    [M, I] = max(A(k:n, k));
+    j = k + I - 1;
+    j_ = A(j,:);
+    
+    
+    A(k,:) = j_;
+    A(j,:) = k_;
+end
