64f0c3b8e22308b3c0b964dccb24b2eb4bb9cae0
Juan Neri <Juanjo.neri@gmail.com>
2017-07-19 23:15:02 -0700

    make simposns rule but not composite

--- HW 3/Homework_3_5.m
+++ HW 3/Homework_3_5.m
@@ -1,11 +1,25 @@
 %% Problem 5
 
-simpson (@cos, pi/32, 0, pi/2)
+repes = 12;
+
+log = zeros(repes, 1);
+for i = 1:repes
+    log(i) = simpson (@cos, pi/(2*2^i), 0, pi/2);
+end
+
+err = ones(repes, 1) - log;
+
+rat = zeros(repes-1, 1);
+for i = 1:repes-1
+    rat(i) = err(i+1)/err(i);
+end
+
+rat
 
 function [ I ] = simpson (f, h, l, r)
 
-    v = l:h:r
-    N = size(v,2)
+    v = l:h:r;
+    N = size(v,2);
     
     I = f(v(1)) + f(v(N));
     I = I + 4*sum(f(v(3:2:N-1)));
