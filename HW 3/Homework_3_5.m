d3c1fcd1499714280d962c969d0998547b592f88
Juan Neri <Juanjo.neri@gmail.com>
2017-07-20 07:54:03 -0700

    use natis algorithm to prove 4th order

--- HW 3/Homework_3_5.m
+++ HW 3/Homework_3_5.m
@@ -1,36 +1,45 @@
 %% Problem 5
 
-repes = 7;
+syms('OK','A','B','N','H','XI0','XI1','XI2','NN','I','X','XI','s','x');
 
-log = zeros(repes, 1);
-for i = 1:repes
-    log(i) = compositeSimpson (i, @cos, pi/(2*2^i), 0, pi/2);
+F = @cos;
+
+OK = 1;
+res = ones(7,1);
+for i=1:7
+
+    A = 0;
+    B = pi/2;
+
+    N = 2^i;
+
+    H = (B-A)/N;
+    XI0 = F(A) + F(B);
+    XI1 = 0;
+    XI2 = 0;
+    NN = N - 1;
+
+    for I = 1:NN
+        X = A + I * H;
+        if rem(I,2) == 0  
+            XI2 = XI2 + F(X);
+        else
+            XI1 = XI1 + F(X);      
+        end
+    end
+
+    XI = (XI0 + 2.0 * XI2 + 4.0 * XI1) * H / 3.0;
+    res(i) = XI;
+    
 end
 
-err = (ones(repes, 1) - log) .* 100
+format long
+err = (res - 1);
 
-rat = zeros(repes-1, 1);
-for i = 1:repes-1
-    rat(i) = err(i+1)/err(i);
+rat = ones(6,1);
+
+for i=1:6
+    rat(i) = err(i) / err(i + 1);
 end
 
 rat
-
-function [ I ] = simpson (f, h, l, r)
-
-    v = l:h:r;
-    N = size(v,2);
-    
-    I = f(v(1)) + f(v(N));
-    I = I + 4*sum(f(v(3:2:N-1)));
-    I = I + 2*sum(f(v(2:2:N-2)));
-    
-    I = h/3 * I;
-end
-
-function [ I ] = compositeSimpson ( k, f, h, l, r)
-
-    I = ( 4*simpson(f, h/(2^k), l, r) - simpson(f, h/(2^(k-1)), l, r) ) / 3;
-    
-end
-
--- Sandbox.m
+++ Sandbox.m
@@ -1,1 +1,43 @@
-cos(1:4)
+syms('OK','A','B','N','H','XI0','XI1','XI2','NN','I','X','XI','s','x');
+
+F = inline(cos(x),'x');
+
+OK = 1;
+res = ones(7,1);
+for i=1:7
+
+    A = 0;
+    B = pi/2;
+
+    N = 2^i;
+
+    H = (B-A)/N;
+    XI0 = F(A) + F(B);
+    XI1 = 0;
+    XI2 = 0;
+    NN = N - 1;
+
+    for I = 1:NN
+        X = A + I * H;
+        if rem(I,2) == 0  
+            XI2 = XI2 + F(X);
+        else
+            XI1 = XI1 + F(X);      
+        end
+    end
+
+    XI = (XI0 + 2.0 * XI2 + 4.0 * XI1) * H / 3.0;
+    res(i) = XI;
+    
+end
+
+format long
+err = (res - 1);
+
+rat = ones(6,1);
+
+for i=1:6
+    rat(i) = err(i) / err(i + 1);
+end
+
+rat
