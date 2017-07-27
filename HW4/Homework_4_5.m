65ce7c8fed0a8ac8c77552e56b4312dc0dc59e37
Juan Neri <Juanjo.neri@gmail.com>
2017-07-26 21:11:35 -0700

    finish?

--- HW4/Homework_4_5.m
+++ HW4/Homework_4_5.m
@@ -2,8 +2,7 @@
 
 syms x
 
-Dn (@f, 1, 1, 2, 1)
-D (@f, 1, 1)
+Dn (@f, 1, 1, 4, 4)
 
 function [ y ] = f (x)
     y = log(x^2 + 1);
@@ -16,10 +15,11 @@ function [ d ] = D ( f, x0, h )
 end
 
 function [ d ] = Dn ( f, x0, h, k, m )
-
     if m == 1
         d = D(f, x0, h/(2^(k-1)));
     else
-        d = ( 2^(m-1) * D(f, x0, h, k, m-1) - D(f, x0, h, k-1, m-1) ) / (2^(m-1) - 1);
+        d = ( 2^(m-1) * Dn(f, x0, h, k, m-1) - Dn(f, x0, h, k-1, m-1) ) / (2^(m-1) - 1);
     end
+    
+    fprintf('D_%i,%i = %f \n', k, m, d );
 end
