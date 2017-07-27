ea0163fb00e63b44f8ec5072effb2561d1f2a8b8
Juan Neri <Juanjo.neri@gmail.com>
2017-07-26 21:06:45 -0700

    make recursive function to find richardson extrapolation

---
+++ HW4/Homework_4_5.m
@@ -0,0 +1,25 @@
+%% Problem 5
+
+syms x
+
+Dn (@f, 1, 1, 2, 1)
+D (@f, 1, 1)
+
+function [ y ] = f (x)
+    y = log(x^2 + 1);
+end
+
+function [ d ] = D ( f, x0, h )
+
+    d = ( f(x0) - f(x0 - h) ) / h;
+
+end
+
+function [ d ] = Dn ( f, x0, h, k, m )
+
+    if m == 1
+        d = D(f, x0, h/(2^(k-1)));
+    else
+        d = ( 2^(m-1) * D(f, x0, h, k, m-1) - D(f, x0, h, k-1, m-1) ) / (2^(m-1) - 1);
+    end
+end
