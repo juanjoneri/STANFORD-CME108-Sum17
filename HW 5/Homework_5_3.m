3ac9d8660bde51bcabd377e755e41789934e3c85
Juan Neri <Juanjo.neri@gmail.com>
2017-08-02 14:58:45 -0700

    yesssssssvuse anonimous functions to minimize alpha in grad descent

--- HW 5/Homework_5_3.m
+++ HW 5/Homework_5_3.m
@@ -14,15 +14,19 @@ A = reshape([x; y],s(1)*s(2),2);
 %Z = banana(x, y);
 %surf(x, y, Z)
 
-
+% 2.1 Select an initial guess of position vector
 X0 = [-1,2];
+% 2.2 and evaluate the grad
+G0 = gradient(X0);
 
+A = fminbnd(@(a) banana(X0 - a.*G0) , 0, 2);
 
 function [z] = banana (X)
     x = X(1); y = X(2);
     z = 100 .* (y - x.^2).^2 + (1 - x.^2);
 end
 
+% 1 Establish the analytical expression for the gradient
 function [Z] = gradient (X)
     x = X(1); y = X(2);
     Z = [2*(200*x^3-200*x*y+x-1) 200*(y-x^2)];
