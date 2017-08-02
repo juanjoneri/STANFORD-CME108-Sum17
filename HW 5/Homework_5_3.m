5407c0e359082a8b74f85f05fb8374fdd25c7516
Juan Neri <Juanjo.neri@gmail.com>
2017-08-02 15:22:16 -0700

    change range of alpha and store inter results

--- HW 5/Homework_5_3.m
+++ HW 5/Homework_5_3.m
@@ -14,24 +14,28 @@ A = reshape([x; y],s(1)*s(2),2);
 % 2.1 Select an initial guess of position vector
 X0 = [-1,2];
 
-[X1, k] = steepest(X0, @banana, @gradient, 1.e-6)
+[X, k] = steepest(X0, @banana, @gradient, 1.e-6)
 
-function [X1, k] = steepest (X0, f, grad, tol)
+function [X, k] = steepest (X0, f, grad, tol)
     
     stop = 0;
     k = 1;
     
     while ~stop
+        % Store all intermediate steps for plt
+        X(k, 1) = X0(1);
+        X(k, 2) = X0(2);
         % 2.2 and evaluate the grad
         G0 = grad(X0);
         % 3 Use matlab to find the value of alpha that minimizes the new pair x, y
-        A = fminbnd(@(a) f(X0 - a.*G0) , 0, 2);
+        A = fminbnd(@(a) f(X0 - a.*G0) , 0, 3);
         % 4 Calculate the new position
         X1 = X0 - A.*G0;
-        fprintf('%f     %f\n', X1(1), X1(2));
         % 5 Calculate the residual to stop when done
-        if (norm(X1 - X0) < tol) || (k > 1000)
-           stop = 1; 
+        if (norm(X1 - X0) < tol) || (k > 2000)
+           stop = 1;
+           X(k, 1) = X0(1);
+           X(k, 2) = X0(2);
         end
         X0 = X1;
         k = k+1;
