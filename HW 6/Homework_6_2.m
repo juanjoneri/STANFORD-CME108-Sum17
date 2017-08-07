1725542a93484fb2d78f2836bbeb5dc97b6435cf
Juan Neri <Juanjo.neri@gmail.com>
2017-08-07 16:51:17 -0700

    need to undestand whats going on but seem to be getting results

--- HW 6/Homework_6_2.m
+++ HW 6/Homework_6_2.m
@@ -1,42 +1,48 @@
 %% Problem 2
 
+bet = 0.18;
 
-V = 1; D = 0.1;
-x_0 = 0; x_L = 1;
-y1_0 = 0; B = 0;
-N = 1024;
+x_0 = 0; x_L = 20;
+N = 2048;
 h = (x_L-x_0)/N;
-y2_guess = [0 .005];
-tol = 0.00001;
-fprintf('\n Iteration C(0) C''(1) \n')
 
-for n = 1:15 % try 15 iterations
+y1_0 = 0; 
+y2_0 = 0;
+y3_guess = [10 20];
+B = 1;
+
+tol = 0.1;
+
+fprintf('\n Iteration    C(0)        C''(1) \n')
+
+for n = 1:50
     
     if n <= 2
-        y2_0(n) = y2_guess(n);
+        y3_0(n) = y3_guess(n);
     end
     
-    [x,y] = ode45(@(t,y) pollution(t,y,V,D),[x_0:h:x_L],[y1_0 y2_0(n)]);
-    y2_L(n) = y(N+1,2);
-    fprintf('%5.0f %16.6f %11.6f \n',n,y2_0(n),y2_L(n))
+    [x,y] = ode45(@(t,y) flow(t, y, bet), 0:.1:20, [y1_0 y2_0 y3_0(n)]);
+    y3_L(n) = y(201,3);
+    fprintf('%5.0f %16.6f %11.6f \n',n,y3_0(n),y3_L(n))
     
     if n > 1
-        if abs(y2_L(n)-B) > tol
-            y2_0(n+1) = y2_0(n-1)+(B-y2_L(n-1))*...
-            (y2_0(n)-y2_0(n-1))/(y2_L(n)-y2_L(n-1));
+        if abs(y3_L(n)-B) > tol
+            y3_0(n+1) = y3_0(n-1)+(B-y3_L(n-1))*(y3_0(n)-y3_0(n-1))/(y3_L(n)-y3_L(n-1));
         else
             break
         end
     end
 end
 
-plot(x,y(:,1),'r-')
+plot(x, y(:,2))
 xlabel('x')
 ylabel('C(x)')
-axis([0 1 -0.018 0.02])
+%axis([0 20 0 20])
 title('Pollution Concentration')
 
-function yprime = pollution(x,y,A1,A2)
-    S = 0.1*exp(-100*(x-0.5)^2);
-    yprime = [y(2); A1/A2*y(2)-S/A2];
+function yp = flow(x, y, bet)
+    yp(1) = y(2);
+    yp(2) = y(3);
+    yp(3) = - y(1).*y(3) - bet.*(1 - y(2).^2);
+    yp = yp';
 end
