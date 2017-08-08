580333461c006edc075b0571e2fb41cc8ebf4a49
Juan Neri <Juanjo.neri@gmail.com>
2017-08-07 21:11:33 -0700

    finish getting the real plot

--- HW 6/Homework_6_2.m
+++ HW 6/Homework_6_2.m
@@ -1,9 +1,9 @@
 %% Problem 2
 
-bet = 0.18;
+bet = 0.3;
 
-x_0 = 0; x_L = 20;
-N = 2048;
+x_0 = 0; x_L = 10;
+N = 500;
 h = (x_L-x_0)/N;
 
 y1_0 = 0; 
@@ -11,20 +11,20 @@ y2_0 = 0;
 y3_guess = [10 20];
 B = 1;
 
-tol = 0.1;
+tol = 0.0001;
 
-fprintf('\n Iteration    C(0)        C''(1) \n')
+fprintf('\nIteration    C(0)        C''(10) \n')
 
 for n = 1:50
-    
+
     if n <= 2
         y3_0(n) = y3_guess(n);
     end
-    
-    [x,y] = ode45(@(t,y) flow(t, y, bet), 0:.1:20, [y1_0 y2_0 y3_0(n)]);
-    y3_L(n) = y(201,3);
-    fprintf('%5.0f %16.6f %11.6f \n',n,y3_0(n),y3_L(n))
-    
+
+    [x,y] = ode45(@(t,y) flow(t, y, bet), x_0:h:x_L, [y1_0 y2_0 y3_0(n)]);
+    y3_L(n) = y(N+1,3);
+    fprintf('%5.0f %16.6f %11.6f \n', n, y3_0(n), y3_L(n))
+
     if n > 1
         if abs(y3_L(n)-B) > tol
             y3_0(n+1) = y3_0(n-1)+(B-y3_L(n-1))*(y3_0(n)-y3_0(n-1))/(y3_L(n)-y3_L(n-1));
@@ -34,11 +34,11 @@ for n = 1:50
     end
 end
 
-plot(x, y(:,2))
-xlabel('x')
-ylabel('C(x)')
-%axis([0 20 0 20])
-title('Pollution Concentration')
+plot(x, y(:,1))
+xlabel('nu')
+ylabel('f(nu)')
+axis([0 10 0 300])
+title('Flow of the fluid')
 
 function yp = flow(x, y, bet)
     yp(1) = y(2);
