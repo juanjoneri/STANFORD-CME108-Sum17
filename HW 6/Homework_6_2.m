fa3dfe3060af164ea07fe2a03f35c5322e743d01
Juan Neri <Juanjo.neri@gmail.com>
2017-08-08 22:24:46 -0700

    finally make it work real result

--- HW 6/Homework_6_2.m
+++ HW 6/Homework_6_2.m
@@ -1,5 +1,5 @@
 %% Problem 2
-
+clear
 bet = 0.3;
 
 x_0 = 0; x_L = 10;
@@ -8,12 +8,12 @@ h = (x_L-x_0)/N;
 
 y1_0 = 0; 
 y2_0 = 0;
-y3_guess = [10 20];
+y3_guess = [0 1];
 B = 1;
 
-tol = 0.0001;
+tol = 0.01;
 
-fprintf('\nIteration    C(0)        C''(10) \n')
+fprintf('\nIteration    C''''(0)        C''(10) \n')
 
 for n = 1:50
 
@@ -21,13 +21,13 @@ for n = 1:50
         y3_0(n) = y3_guess(n);
     end
 
-    [x,y] = ode45(@(t,y) flow(t, y, bet), x_0:h:x_L, [y1_0 y2_0 y3_0(n)]);
-    y3_L(n) = y(N+1,3);
-    fprintf('%5.0f %16.6f %11.6f \n', n, y3_0(n), y3_L(n))
+    [x,y] = ode45(@(t,y) flow(t, y, bet), 0:0.01:10, [y1_0 y2_0 y3_0(n)]);
+    y2_L(n) = y(end, 2);
+    fprintf('%5.0f %16.6f %11.6f \n', n, y3_0(n), y2_L(n))
 
     if n > 1
-        if abs(y3_L(n)-B) > tol
-            y3_0(n+1) = y3_0(n-1)+(B-y3_L(n-1))*(y3_0(n)-y3_0(n-1))/(y3_L(n)-y3_L(n-1));
+        if abs(y2_L(n)-B) > tol
+            y3_0(n+1) = y3_0(n-1)+(B-y2_L(n-1))*(y3_0(n)-y3_0(n-1))/(y2_L(n)-y2_L(n-1));
         else
             break
         end
@@ -35,9 +35,12 @@ for n = 1:50
 end
 
 plot(x, y(:,1))
-xlabel('nu')
-ylabel('f(nu)')
-axis([0 10 0 300])
+hold on
+plot(x, y(:,2))
+hold on
+plot(x, y(:,3))
+
+xlabel('\eta')
 title('Flow of the fluid')
 
 function yp = flow(x, y, bet)
