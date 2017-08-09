7ad8c14dbb8a2d5fb2a3888ebd4268d2e053a8e2
Juan Neri <Juanjo.neri@gmail.com>
2017-08-08 23:10:39 -0700

    decrease tol and make output nicer

--- HW 6/Homework_6_2.m
+++ HW 6/Homework_6_2.m
@@ -1,46 +1,62 @@
 %% Problem 2
-clear
-bet = 0.3;
+clear; warning('off','all');
 
-x_0 = 0; x_L = 10;
-N = 500;
-h = (x_L-x_0)/N;
-
-y1_0 = 0; 
-y2_0 = 0;
-y3_guess = [0 1];
-B = 1;
-
-tol = 1e-8;
-
-fprintf('\nIteration    C''''(0)        C''(10) \n')
-
-for n = 1:15
-
-    if n <= 2
-        y3_0(n) = y3_guess(n);
-    end
-
-    [x,y] = ode45(@(t,y) flow(t, y, bet), 0:0.01:10, [y1_0 y2_0 y3_0(n)]);
-    y2_L(n) = y(end, 2);
-    fprintf('%5.0f %16.6f %11.6f \n', n, y3_0(n), y2_L(n))
-
-    if n > 1
-        if abs(y2_L(n)-B) > tol
-            y3_0(n+1) = y3_0(n-1)+(B-y2_L(n-1))*(y3_0(n)-y3_0(n-1))/(y2_L(n)-y2_L(n-1));
-        else
-            break
-        end
-    end
-end
+[x, y] = bvp(0.18);
 
 plot(x, y(:,1))
 hold on
 plot(x, y(:,2))
 
 xlabel('Scaled distance normal to the wall (\eta)')
-legend('f(\eta)','f''(\eta)')
-title('Flow of the fluid, boundary layer profile')
+legend('f(\eta)','U(\eta)')
+title('Boundary layer profile (\beta = 0.18)')
+figure
+
+[x, y] = bvp(0.3);
+
+plot(x, y(:,1))
+hold on
+plot(x, y(:,2))
+
+xlabel('Scaled distance normal to the wall (\eta)')
+legend('f(\eta)','U(\eta)')
+title('Boundary layer profile (\beta = 0.3)')
+
+function [x,y] = bvp(bet)
+
+    x_0 = 0; x_L = 10;
+    N = 512;
+    h = (x_L-x_0)/N;
+
+    y1_0 = 0; 
+    y2_0 = 0;
+    y3_guess = [1 0.9];
+    B = 1;
+    tol = 1e-8;
+
+    fprintf('\n It        f''''(0)      f''(10) \n')
+    fprintf(' -------   ---------   ---------   \n')
+
+    for n = 1:15
+
+        if n <= 2
+            y3_0(n) = y3_guess(n);
+        end
+
+        [x,y] = ode45(@(t,y) flow(t, y, bet), x_0:h:x_L, [y1_0 y2_0 y3_0(n)]);
+        y2_L(n) = y(end, 2);
+        fprintf('%2i %16.6f %11.6f \n', n, y3_0(n), y2_L(n))
+
+        if n > 1
+            if abs(y2_L(n)-B) > tol
+                y3_0(n+1) = y3_0(n-1)+(B-y2_L(n-1))*(y3_0(n)-y3_0(n-1))/(y2_L(n)-y2_L(n-1));
+            else
+                break
+            end
+        end
+    end
+
+end
 
 function yp = flow(x, y, bet)
     yp(1) = y(2);
